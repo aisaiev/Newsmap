60234c74bffed8ef4eff14acc9640dbfb233d093
Anton Isaiev <antonio.de.tores@gmail.com>
2016-10-30 11:41:34 +0200

    Initial commit

---
+++ includes/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+    /**
+     * Global constants
+     */
+
+    // your database's name
+    define("DATABASE", "database");
+
+    // your database's password
+    define("PASSWORD", "passsword");
+
+    // your database's server
+    define("SERVER", "server");
+
+    // your database's username
+    define("USERNAME", "username");
+
+?>
